1b71eab824017ba9de41accbb6a6a447d27827a9
Gino Hong <ginohong7@gmail.com>
2022-06-08 01:46:56 +0000

    Added function for start case and implemented it. Hard coded the icon for the character, Yae Miko, because only her icon is not found in the database. #1

--- js/main.js
+++ js/main.js
@@ -6,13 +6,29 @@ xhr.responseType = 'json';
 xhr.addEventListener('load', function () {
   for (var i = 0; i < xhr.response.length; i++) {
     var $character = document.createElement('div');
+    $character.className = 'character';
     var $img = document.createElement('img');
     $img.src = 'https://api.genshin.dev/characters/' + xhr.response[i] + '/icon';
+    $img.alt = xhr.response[i];
+    if ($img.alt === 'yae-miko') {
+      $img.src = 'https://api.genshin.dev/characters/yae-miko/icon-big';
+      $img.className = 'smole';
+    }
     var $p = document.createElement('p');
-    $p.textContent = xhr.response[i];
+    $p.textContent = startCase(xhr.response[i]);
     $character.appendChild($img);
     $character.appendChild($p);
     $characterlist.appendChild($character);
   }
 });
 xhr.send();
+
+function startCase(str) {
+  str = str.split('-');
+
+  for (var i = 0, x = str.length; i < x; i++) {
+    str[i] = str[i][0].toUpperCase() + str[i].substr(1);
+  }
+
+  return str.join(' ');
+}
